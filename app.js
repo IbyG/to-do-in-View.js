ce56a3db3bd53cc0c3ebd30ca98b0250cd7416f2
IbyG <ibrahimgeorge1@gmail.com>
2023-12-31 19:16:04 +1100

    added categories functionality for phase 1

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ new Vue({
   el: '#app',
   data: {
     newTask: '',
+    showDragHandle: false,
     tasks: [
-      { id: 1, text: 'Learn Vue.js', completed: false },
-      { id: 2, text: 'Build a to-do app', completed: true },
-      // Add more tasks as needed
-    ]
+      { id: 1, text: 'Learn Vue.js', completed: false, category: 'To Do' },
+      { id: 2, text: 'Build a to-do app', completed: true, category: 'In Progress' },
+      { id: 3, text: 'Implement Kanban view', completed: false, category: 'Done' },
+      // Add more tasks as needed with different categories
+    ],
+    categories: ['To Do', 'In Progress', 'Done'], // Define the possible categories
   },
   methods: {
     addTask: function() {
@@ -16,7 +19,8 @@ new Vue({
         const newTask = {
           id: Date.now(),
           text: this.newTask,
-          completed: false
+          completed: false,
+          category: 'To Do', // Default category
         };
         this.tasks.push(newTask);
         this.newTask = '';
@@ -30,15 +34,17 @@ new Vue({
     },
     allowDrop: function(event) {
       event.preventDefault();
+      this.showDragHandle = true;
     },
     drop: function(event) {
       event.preventDefault();
+      this.showDragHandle = false;
       const taskId = parseInt(event.dataTransfer.getData('text/plain'), 10);
       const taskIndex = this.tasks.findIndex(task => task.id === taskId);
       const droppedTask = this.tasks.splice(taskIndex, 1)[0];
       const newIndex = Array.from(event.target.parentNode.children).indexOf(event.target);
       this.tasks.splice(newIndex, 0, droppedTask);
-    }
-  }
+    },
+  },
 });
 
